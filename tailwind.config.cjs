2ba51d339a092d4cf194107064a8d16c39f7230a
ViktorMo <34257555+Mo-Love@users.noreply.github.com>
2025-11-17 22:02:27 +0200

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+js
+module.exports = {
+content: ['./index.html', './src/**/*.{js,jsx}'],
+theme: {
+extend: {
+colors: {
+primary: '#0F172A',
+accent: '#6D28D9',
+gradientStart: '#0ea5e9',
+gradientEnd: '#7c3aed'
+}
+}
+},
+plugins: []
+}
